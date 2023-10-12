bd556411fc59e149b7f69ff422634613ecd281e0
Binsal <96328531+Binsal@users.noreply.github.com>
2023-10-12 08:17:34 +0530

    added search bar

--- index.js
+++ index.js
@@ -1,36 +1,54 @@
 import {getRecipeCard} from "./getRecipeCard.js";
 import { getCuisineCard } from "./getCuisine.js";
 
+const cardParentContainer = document.querySelector(".main");
+const cuisineParentContainer = document.querySelector(".cuisine-filter");
+const searchBox = document.querySelector(".input");
 
-// let searchValue = "";
-// let filteredArrOfCusines = [];
+const RECIPEURL = "https://recipeapi.prakashsakari.repl.co/api/recipes";
+const CUISINEURL =
+  "https://recipeapi.prakashsakari.repl.co/api/recipes/cuisines";
 
-const CardParentContainer = document.querySelector(".main");
-const CuisineParentContainer = document.querySelector(".cusine-filter");
-const searchBox = document.querySelector(".input")
+let searchValue = "";
+let filteredArrOfRecipes = [];
 
 const createElement = (element)=> document.createElement(element); 
 
-const RECIPEURL = "https://recipeapi.prakashsakari.repl.co/api/recipes";
-const CUISINEURL ="https://recipeapi.prakashsakari.repl.co/api/recipes/cuisines";
-
-const getData = async (URL) =>{
-    try{
-        const {data} = await axios.get(URL);
-        return data;
+const getData = async (URL) => {
+    try {
+      const { data } = await axios.get(URL);
+      return data;
+    } catch (err) {
+      console.log(err);
     }
-    catch(err){
-        console.log(err);
-    }
-};
+  };
+  
 
 const recipes = await getData(RECIPEURL);
 const cuisines = await getData(CUISINEURL);
 
-// searchBox.addEventListener("keyup",searchInputHandler);
+const getFilteredData = () => {
+    filteredArrOfRecipes =
+    searchValue?.length > 0
+      ? recipes.filter((recipe) =>
+          recipe.TranslatedRecipeName.toLowerCase().includes(searchValue)
+        )
+      : recipes;
 
-getRecipeCard(recipes,CardParentContainer,createElement);
-getCuisineCard(cuisines,CuisineParentContainer,createElement);
+    return filteredArrOfRecipes;
+};
+
+const searchInputHandler = (event) => {
+    searchValue = event.target.value.toLowerCase();
+    const filteredData = getFilteredData();
+    cardParentContainer.innerHTML = "";
+    getRecipeCard(filteredData, cardParentContainer, createElement);
+  };
+
+searchBox.addEventListener("keyup", searchInputHandler);
+
+getRecipeCard(recipes, cardParentContainer, createElement);
+getCuisineCard(cuisines, cuisineParentContainer, createElement);
 
 
 
